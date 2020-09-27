e776394ff74512a67ad1443656cd1a5b44bc3658
Puneeth <puneet.shivram@gmail.com>
2020-09-27 17:39:57 +0530

    Added chart tooltip label color

--- src/components/Rechart/index.js
+++ src/components/Rechart/index.js
@@ -11,16 +11,16 @@ function Chart({data}){
                     margin={{ top: 10, right: 0, left: 20, bottom: 0 }}>
                     <defs>
                         <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-                        <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
-                        <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
+                            <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
+                            <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
                         </linearGradient>
                         <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-                        <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8}/>
-                        <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>
+                            <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8}/>
+                            <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>
                         </linearGradient>
                         <linearGradient id="colorRed" x1="0" y1="0" x2="0" y2="1">
-                        <stop offset="5%" stopColor="red" stopOpacity={0.8}/>
-                        <stop offset="95%" stopColor="red" stopOpacity={0}/>
+                            <stop offset="5%" stopColor="#ce3734" stopOpacity={0.8}/>
+                            <stop offset="95%" stopColor="#ce3734" stopOpacity={0}/>
                         </linearGradient>
                     </defs>
                     <XAxis dataKey="date" />
@@ -29,7 +29,7 @@ function Chart({data}){
                     <Tooltip />
                     <Area type="monotone" dataKey="totalconfirmed" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
                     <Area type="monotone" dataKey="totalrecovered" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
-                    <Area type="monotone" dataKey="totaldeceased" stroke="#82ca9d" fillOpacity={1} fill="url(#colorRed)" />
+                    <Area type="monotone" dataKey="totaldeceased" stroke="#ce3734" fillOpacity={1} fill="url(#colorRed)" />
                 </AreaChart>
             </ResponsiveContainer>
         </>
